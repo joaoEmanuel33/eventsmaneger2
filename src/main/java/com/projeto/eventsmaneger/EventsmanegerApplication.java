fb57eab18b088b727e0ba7928b1ed4dd3323403b
JOÃO EMANUEL GUESSI CAMARGO <joao_e_camargo@estudante.sesisenai.org.br>
2025-08-14 17:20:04 -0300

    projeto curso

---
+++ src/main/java/com/projeto/eventsmaneger/EventsmanegerApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.eventsmaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsmanegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsmanegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/eventsmaneger/EventsmanegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.eventsmaneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsmanegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
